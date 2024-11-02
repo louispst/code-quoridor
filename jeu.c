8a9776c3ac16f153c0011bb052b71e48457ac34c
louispst <louis.poinsot@edu.ece.fr>
2024-11-02 16:03:06 +0100

    Update jeu.c

--- jeu.c
+++ jeu.c
@@ -1,2 +1,2 @@
-//complémente les fonctions définies dans jeu.h, y compris la logique principale du jeu, la gestion des tours, et les conditions de victoire.
+//complémente les fonctions définies dans jeu.h, y compris la logique principale du jeu, la gestion des tours, et les conditions de victoire. Pour Dory
 
