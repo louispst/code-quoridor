f53cd88a19d90babe79a07fcc4d45bb2b37199f7
louispst <louis.poinsot@edu.ece.fr>
2024-11-02 16:06:44 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,3 +1,6 @@
+//Le point d'entrée du programme. Il initialise le jeu et appelle les sous-programmes nécessaires pour gérer le déroulement de la partie. Pour Juliette
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
