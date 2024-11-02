16fd3f1f9dd1d0949e90a0a76a2510add3c256a4
louispst <louis.poinsot@edu.ece.fr>
2024-11-02 16:04:44 +0100

    Update joueur.h

--- joueur.h
+++ joueur.h
@@ -1,1 +1,3 @@
+/*Déclare les structures représentant les joueurs, ainsi que les prototypes des fonctions pour gérer les actions des joueurs, 
+comme le déplacement ou la construction de barrières. Pour Alexandre*/
 
