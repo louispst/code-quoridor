f88e399b7d2962af0d7b4cc17a85170a46cb86fc
louispst <louis.poinsot@edu.ece.fr>
2024-11-03 10:35:42 +0100

    Update plateau.h

--- plateau.h
+++ plateau.h
@@ -1,2 +1,2 @@
 /*Contient les définitions de la structure du plateau, ainsi que les prototypes des fonctions pour manipuler le plateau de jeu,
-comme l'affichage et la mise à jour. Pour juliette*/
+comme l'affichage et la mise à jour. Pour Louis*/
