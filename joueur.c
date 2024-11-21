0e2b1127b3eb0c70f890c40c16ffd60213dc4e4e
juliettelobut <juliette.lobut@edu.ece.fr>
2024-11-21 10:10:18 +0100

    Update joueur.c

--- joueur.c
+++ joueur.c
@@ -1,2 +1,12 @@
-//Implémente les fonctions définies dans joueur.h, gérant les actions des joueurs et leurs interactions avec le jeu. Pour Alexandre
+//Implémente les fonctions définies dans joueur.h, gérant les actions des joueurs et leurs interactions avec le jeu. Pour Juliette
+/ Fonction pour afficher le menu d'actions d'un joueur
+void afficher_menu_joueur() {
+    printf("\n--- Menu du joueur ---\n");
+    printf("1. Deplacer le pion\n");
+    printf("2. Poser une barrire\n");
+    printf("3. Passer son tour\n");
+    printf("4. Annuler le coup\n");
+    printf("Entrez votre choix: ");
+}
+
 
