c7307078d6dd60108b608b69f3e95e6f93bd0759
juliettelobut <juliette.lobut@edu.ece.fr>
2024-11-21 10:39:05 +0100

    Update joueur.c

--- joueur.c
+++ joueur.c
@@ -8,5 +8,19 @@ void afficher_menu_joueur() {
     printf("4. Annuler le coup\n");
     printf("Entrez votre choix: ");
 }
-
+// Fonction pour afficher le menu des directions
+void afficher_menu_direction() {
+    printf("\nOù voulez-vous aller ?\n");
+    printf("1. Aller en haut\n");
+    printf("2. Aller en bas\n");
+    printf("3. Aller à gauche\n");
+    printf("4. Aller à droite\n");
+    printf("Entrez votre choix : ");
+}
+// Fonction pour passer au joueur suivant
+void passer_au_suivant(t_joueur joueurs[], int *joueur_courant, int nombre_joueurs) {
+    *joueur_courant = (*joueur_courant + 1) % nombre_joueurs;  // Passer au joueur suivant
+    printf("\nC'est au tour de %s de jouer !\n", joueurs[*joueur_courant].prenom);
+    afficher_menu_joueur();
+}
 
