9f43cd612631383b01aecae6f4a23898ba3c8acb
alexcattann27 <acattan2701@gmail.com>
2024-11-21 11:00:08 +0100

    jeu.h

--- jeu.h
+++ jeu.h
@@ -1,5 +1,5 @@
 //Contient les constantes et structures liées au jeu, ainsi que les prototypes des fonctions pour initialiser, gérer et terminer le jeu. Pour Dory
 #ifndef
 #define
-
+void deplacer_pion(t_joueur *joueur)
 #endif
