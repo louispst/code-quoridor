166446b10f97b060a8fdbcd432072423bf95322d
alexcattann27 <acattan2701@gmail.com>
2024-11-21 11:04:45 +0100

    menu_principal.h

--- menu_principal.h
+++ menu_principal.h
@@ -1,1 +1,10 @@
-
+#ifndef
+#define
+void afficher_menu();
+void demander_nombre_de_joueurs(int *nombre_joueurs, t_joueur joueurs[]);
+int choisir_joueur_au_hasard(int nombre_joueurs);
+void choisir_pions(t_joueur joueurs[], int nombre_joueurs);
+void afficher_aide();
+void afficher_scores(t_joueur joueurs[], int nombre_joueurs);
+int choisir_joueur_au_hasard(int nombre_joueurs);
+#endif
