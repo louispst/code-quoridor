5e727dddc81ffce454f91fb97c10769126957455
alexcattann27 <acattan2701@gmail.com>
2024-11-21 11:15:00 +0100

    plateau.h

--- plateau.h
+++ plateau.h
@@ -1,2 +1,7 @@
 /*Contient les définitions de la structure du plateau, ainsi que les prototypes des fonctions pour manipuler le plateau de jeu,
 comme l'affichage et la mise à jour. Pour Louis*/
+#ifndef
+#define
+void afficher_plateau(t_joueur joueurs[], int nombre_joueurs);
+void positionner_joueurs(t_joueur joueurs[], int nombre_joueurs);
+#endif
